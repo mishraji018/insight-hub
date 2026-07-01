d3d9b2cdbe6502e349febdbe019b75ab50acf37c
Mishra ji <pmishra2084@gmail.com>
2026-07-01 20:22:17 +0530

    Fix ASGI to use production settings

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
 # Initialize Django ASGI application early to ensure AppRegistry is populated
 # before importing consumers/models.
 django_asgi_app = get_asgi_application()
